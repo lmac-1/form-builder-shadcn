6e86248b43ba53d0a49a0ed078c6f7d2cce182b2
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-17 11:54:50 -0500

    run prettier

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,10 +5,10 @@ import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav className="flex items-center justify-between border-b border-border h-[60px] px-4 py-2">
+    <div className="flex max-h-screen min-h-screen min-w-full flex-col bg-background">
+      <nav className="flex h-[60px] items-center justify-between border-b border-border px-4 py-2">
         <Logo />
-        <div className="flex gap-4 items-center">
+        <div className="flex items-center gap-4">
           <ThemeSwitcher />
           <UserButton afterSignOutUrl="/sign-in" />
         </div>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link
       href="/"
-      className="font-bold font-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 hover:cursor-pointer text-transparent bg-clip-text"
+      className="font-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text font-bold text-transparent hover:cursor-pointer"
     >
       PageForm
     </Link>
