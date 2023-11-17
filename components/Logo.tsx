620fa47029fc27b887aac73d5a39473c0838c1ee
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-17 11:59:48 -0500

    updates theme

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link
       href="/"
-      className="font-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text font-bold text-transparent hover:cursor-pointer"
+      className="bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-3xl font-bold text-transparent hover:cursor-pointer"
     >
       PageForm
     </Link>
